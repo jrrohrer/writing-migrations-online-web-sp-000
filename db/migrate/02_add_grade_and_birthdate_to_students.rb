cdc9a1c525d9b604f75f1dfccc57fce2e17c8595
Jessica Rohrer <jessie.rohrer@gmail.com>
2021-01-06 02:46:55 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,5 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    create_table :artists do |t|
-      t.string :name
+    add_column :artists, :favorite_food, :string
   end
 end
